2778a3dda848f2594bded13b82ade59bb5461f8b
Erik Clevenger <eclevenger@cnm.edu>
2018-05-07 09:08:30 -0600

    wooomp woooomp whaaaaaaa

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 function MoveIt (){
 
+	//didn't get to finish this up...
+
 	let l= 'pixelLeft', t= 'pixelTop';
 	let arr = [ [l,50], [l,-50], [t,50], [t, -50] ];
 	let n= Math.round(Math.random() * arr.length);
 
-	document.getElementById("move")[arr[n][0]]= arr[n][1];
+	document.getElementById("clicky")[arr[n][0]]= arr[n][1];
 }
